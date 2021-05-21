5ef0625fe03a7983d553978aa96816a14265fcc4
brachy84 <brachy84@gmail.com>
2021-05-21 15:33:37 +0200

    fix progress bar and add rei method

--- src/main/java/brachy84/brachydium/gui/widgets/ProgressBarWidget.java
+++ src/main/java/brachy84/brachydium/gui/widgets/ProgressBarWidget.java
@@ -1,13 +1,14 @@
 package brachy84.brachydium.gui.widgets;
 
-import brachy84.brachydium.gui.api.MoveDirection;
-import brachy84.brachydium.gui.api.ProgressTexture;
-import brachy84.brachydium.gui.api.TextureArea;
-import brachy84.brachydium.gui.api.Widget;
+import brachy84.brachydium.gui.api.*;
+import brachy84.brachydium.gui.impl.GuiHelperImpl;
 import brachy84.brachydium.gui.math.AABB;
 import brachy84.brachydium.gui.math.Point;
+import brachy84.brachydium.gui.math.Size;
+import me.shedaniel.rei.api.widgets.Widgets;
 import net.minecraft.client.util.math.MatrixStack;
 
+import java.util.List;
 import java.util.function.DoubleSupplier;
 
 public class ProgressBarWidget extends Widget {
@@ -34,24 +35,43 @@ public class ProgressBarWidget extends Widget {
 
     @Override
     public void draw(MatrixStack matrices, Point mousePos, float delta) {
+        draw(guiHelper, pos);
+    }
+
+    public void draw(GuiHelper helper, Point point) {
         // always draw empty
-        guiHelper.drawTextureArea(empty, pos, size);
+        helper.drawTextureArea(empty, point, size);
         float u0 = 0, u1 = 1, v0 = 0, v1 = 1;
+        float width = size.width, height = size.height;
         switch (dir) {
-            case RIGHT: {
+            case RIGHT:
                 u1 = (float) progress.getAsDouble();
-            }
-            case LEFT: {
+                width *= u1;
+                break;
+            case LEFT:
                 u0 = (float) (1 - progress.getAsDouble());
-            }
-            case UP: {
+                width *= 1 - u0;
+                break;
+            case UP:
                 v0 = (float) (1 - progress.getAsDouble());
-            }
-            case DOWN: {
+                height *= 1 - v0;
+                break;
+            case DOWN:
                 v1 = (float) progress.getAsDouble();
-            }
+                height *= v1;
+                break;
         }
         TextureArea partFull = full.getSubArea(u0, v0, u1, v1);
-        guiHelper.drawTextureArea(partFull, pos, size);
+        helper.drawTextureArea(partFull, point, new Size(width, height));
+    }
+
+    @Override
+    public void getReiWidgets(List<me.shedaniel.rei.gui.widget.Widget> widgets, Point origin) {
+        GuiHelperImpl guiHelper = new GuiHelperImpl(new MatrixStack());
+        me.shedaniel.rei.gui.widget.Widget renderer = Widgets.createDrawableWidget(((helper, matrices, mouseX, mouseY, delta) -> {
+            guiHelper.setMatrixStack(matrices);
+            draw(guiHelper, origin.add(relativPos));
+        }));
+        widgets.add(renderer);
     }
 }
