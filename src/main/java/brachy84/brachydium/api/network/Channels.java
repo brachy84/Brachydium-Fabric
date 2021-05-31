9500e8ed991f829c5fa66c7f6838e975bfae164c
brachy84 <brachy84@gmail.com>
2021-05-31 09:58:41 +0200

    add channels class

---
+++ src/main/java/brachy84/brachydium/api/network/Channels.java
@@ -0,0 +1,9 @@
+package brachy84.brachydium.api.network;
+
+import brachy84.brachydium.Brachydium;
+import net.minecraft.util.Identifier;
+
+public class Channels {
+
+    public static final Identifier UPDATE_WORKING_STATE = Brachydium.id("update_working_state");
+}
