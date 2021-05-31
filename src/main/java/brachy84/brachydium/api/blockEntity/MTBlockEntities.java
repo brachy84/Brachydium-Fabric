e47e65297b6a4798d76d841e4603e0c9ad8d69a4
brachy84 <brachy84@gmail.com>
2021-05-31 11:12:38 +0200

    update test classes

--- src/main/java/brachy84/brachydium/api/blockEntity/MTBlockEntities.java
+++ src/main/java/brachy84/brachydium/api/blockEntity/MTBlockEntities.java
@@ -3,18 +3,17 @@ package brachy84.brachydium.api.blockEntity;
 import brachy84.brachydium.api.BrachydiumApi;
 import brachy84.brachydium.api.energy.Voltages;
 import brachy84.brachydium.api.recipe.RecipeTables;
+import brachy84.brachydium.api.render.Textures;
 
 import static brachy84.brachydium.Brachydium.*;
 
 public class MTBlockEntities {
 
-    public static WorkableMetaBlockEntity TEST_TILE;
-    public static WorkableMetaBlockEntity ANOTHER_TILE;
-    public static GuiTestBlockEntity GUI_TEST_TILE;
+    public static WorkableMetaBlockEntity ALLOY_SMELTER;
+    public static WorkableMetaBlockEntity MIXER;
 
     public static void init() {
-        TEST_TILE = BrachydiumApi.registerTileEntity(new WorkableMetaBlockEntity(id("test_tile"), Voltages.LV, RecipeTables.ALLOYER_RECIPES));
-        ANOTHER_TILE = BrachydiumApi.registerTileEntity(new WorkableMetaBlockEntity(id("another_machine"), Voltages.LuV, RecipeTables.ALLOYER_RECIPES));
-        GUI_TEST_TILE = BrachydiumApi.registerTileEntity(new GuiTestBlockEntity(id("gui_test_tile")));
+        MIXER = BrachydiumApi.registerTileEntity(new WorkableMetaBlockEntity(id("mixer"), Voltages.LV, Textures.MIXER, RecipeTables.MIXER_RECIPES));
+        ALLOY_SMELTER = BrachydiumApi.registerTileEntity(new WorkableMetaBlockEntity(id("alloy_smelter"), Voltages.LV, Textures.ALLOY_SMELTER, RecipeTables.ALLOYER_RECIPES));
     }
 }
--- src/main/java/brachy84/brachydium/common/recipes/TestRecipes.java
+++ src/main/java/brachy84/brachydium/common/recipes/TestRecipes.java
@@ -2,16 +2,18 @@ package brachy84.brachydium.common.recipes;
 
 import brachy84.brachydium.api.recipe.RecipeTables;
 import brachy84.brachydium.api.tag.Tags;
-import brachy84.brachydium.api.util.Util;
 import brachy84.brachydium.common.Materials;
+import net.minecraft.fluid.Fluids;
 import net.minecraft.item.Items;
+
+import static brachy84.brachydium.api.tag.TagDictionary.*;
 
 public class TestRecipes {
 
     public static void init() {
         RecipeTables.ALLOYER_RECIPES.recipeBuilder("special_iron")
-                .input(Tags.INGOT, Materials.Aluminium, 1)
-                .input(Tags.DUST, Materials.Copper, 1)
+                .input(Ingot, Materials.Aluminium, 1)
+                .input(Dust, Materials.Copper, 1)
                 .output(Items.IRON_INGOT, 2)
                 .duration(40)
                 .EUt(100)
@@ -20,6 +22,15 @@ public class TestRecipes {
         RecipeTables.ALLOYER_RECIPES.recipeBuilder("special_gold")
                 .input(Materials.Aluminium.dust(1), Materials.Copper.ingot(1))
                 .output(Items.GOLD_INGOT, 2)
+                .duration(40)
+                .EUt(28)
+                .buildAndRegister();
+
+        RecipeTables.MIXER_RECIPES.recipeBuilder()
+                .input(Dust, Materials.Copper, 1)
+                .input(Dust, Materials.Aluminium, 1)
+                .fluidInput(Fluids.WATER, 81000)
+                .output(Items.REDSTONE, 2)
                 .duration(40)
                 .EUt(28)
                 .buildAndRegister();
