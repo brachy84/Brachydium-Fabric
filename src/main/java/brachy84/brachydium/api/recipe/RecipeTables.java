1bfb83374adbea25e4f3ffd39616b63bc8afd614
brachy84 <brachy84@gmail.com>
2021-05-31 11:21:07 +0200

    test recipe tables

--- src/main/java/brachy84/brachydium/api/recipe/RecipeTables.java
+++ src/main/java/brachy84/brachydium/api/recipe/RecipeTables.java
@@ -4,11 +4,12 @@ import brachy84.brachydium.api.recipe.builders.SimpleRecipeBuilder;
 
 public class RecipeTables {
 
-    public static final RecipeTable<SimpleRecipeBuilder> ALLOYER_RECIPES;
+    public static final RecipeTable<SimpleRecipeBuilder> ALLOYER_RECIPES = new RecipeTable<>("alloy_smelter", 1, 2, 1, 1, 0, 0, 0, 0, new SimpleRecipeBuilder().duration(20).EUt(2));
+    public static final RecipeTable<SimpleRecipeBuilder> MIXER_RECIPES = new RecipeTable<>("mixer", 1, 4, 0, 1, 0, 2, 0, 1, new SimpleRecipeBuilder().duration(20).EUt(2));
+    public static final RecipeTable<SimpleRecipeBuilder> MACERATOR_RECIPES = new RecipeTable<>("macerator", 1, 1, 1, 3, 0, 0, 0, 0, new SimpleRecipeBuilder().duration(20).EUt(2));
+    public static final RecipeTable<SimpleRecipeBuilder> CHEMICAL_REACTOR_RECIPES = new RecipeTable<>("chemical_reactor", 0, 2, 0, 2, 0, 3, 0, 3, new SimpleRecipeBuilder().duration(20).EUt(2));
+    public static final RecipeTable<SimpleRecipeBuilder> CLUSTER_MILL = new RecipeTable<>("cluster_mill", 1, 1, 1, 1, 0, 0, 0, 0, new SimpleRecipeBuilder().duration(20).EUt(2));
 
-    static {
-        ALLOYER_RECIPES = new RecipeTable<>("alloy_smelter", 2, 6, 1, 2, 0, 3, 0, 0, new SimpleRecipeBuilder().duration(400).EUt(2));
+    public static void init() {
     }
-
-    public static void init() {}
 }
