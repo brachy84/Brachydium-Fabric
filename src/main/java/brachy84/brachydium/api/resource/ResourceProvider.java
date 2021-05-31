7f17bdb3d9ce1fb25556c2ba0acfe7ca47c0dc89
brachy84 <brachy84@gmail.com>
2021-05-31 06:14:27 +0200

    rename model provider

---
+++ src/main/java/brachy84/brachydium/api/resource/ResourceProvider.java
@@ -0,0 +1,34 @@
+package brachy84.brachydium.api.resource;
+
+import brachy84.brachydium.Brachydium;
+import brachy84.brachydium.api.render.MbeHolderModel;
+import net.fabricmc.fabric.api.client.model.ModelProviderContext;
+import net.fabricmc.fabric.api.client.model.ModelProviderException;
+import net.fabricmc.fabric.api.client.model.ModelResourceProvider;
+import net.minecraft.client.render.model.UnbakedModel;
+import net.minecraft.resource.ResourceManager;
+import net.minecraft.util.Identifier;
+import org.jetbrains.annotations.Nullable;
+
+public class ResourceProvider implements ModelResourceProvider {
+
+    public static final MbeHolderModel META_BLOCK_ENTITY_MODEL = new MbeHolderModel();
+
+    private final ResourceManager manager;
+
+    public ResourceProvider(ResourceManager manager) {
+        this.manager = manager;
+    }
+
+    @Override
+    public @Nullable UnbakedModel loadModelResource(Identifier identifier, ModelProviderContext modelProviderContext) {
+        if(identifier.getNamespace().equals(Brachydium.MOD_ID)) {
+            if(identifier.getPath().equals("block/generic_mbe") || identifier.getPath().startsWith("item/mbe/")) {
+                return META_BLOCK_ENTITY_MODEL;
+            }
+        }
+        if(identifier.equals(Brachydium.id("block/generic_mbe")))
+            return META_BLOCK_ENTITY_MODEL;
+        return null;
+    }
+}
