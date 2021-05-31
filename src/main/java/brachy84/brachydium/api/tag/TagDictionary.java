b732cd84785e758ee6cc08374369eab01f00e3bb
brachy84 <brachy84@gmail.com>
2021-05-31 06:30:38 +0200

    added tag entries

--- src/main/java/brachy84/brachydium/api/tag/TagDictionary.java
+++ src/main/java/brachy84/brachydium/api/tag/TagDictionary.java
@@ -1,18 +1,23 @@
 package brachy84.brachydium.api.tag;
 
 import brachy84.brachydium.api.item.CountableIngredient;
+import brachy84.brachydium.api.material.Material;
 
 public class TagDictionary {
 
     public static final long M = 81000L;
 
     public static final Entry Ingot;
+    public static final Entry Dust;
+    public static final Entry SmallDust;
     public static final Entry Block;
     public static final Entry Nugget;
     public static final Entry Plate;
 
     static {
         Ingot = Entry.of("ingot", M);
+        Dust = Entry.of("dust", M);
+        SmallDust = Entry.of("small_dust", M / 9);
         Block = Entry.of("block", M * 9);
         Nugget = Entry.of("nugget", M / 9);
         Plate = Entry.of("plate", M * 2);
@@ -40,6 +45,10 @@ public class TagDictionary {
 
         public CountableIngredient unify(int amount) {
             return null;
+        }
+
+        public String getStringTag(Material material) {
+            return String.format("c:%s_%s", material.getName(), tagName);
         }
     }
 }
