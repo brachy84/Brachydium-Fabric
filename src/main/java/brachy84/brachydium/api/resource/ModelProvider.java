7dc15d48564565802dd329b3dbe3c06eb6d3a700
brachy84 <brachy84@gmail.com>
2021-05-31 06:29:20 +0200

    new model provider

--- src/main/java/brachy84/brachydium/api/resource/ModelProvider.java
+++ src/main/java/brachy84/brachydium/api/resource/ModelProvider.java
@@ -1,27 +1,19 @@
 package brachy84.brachydium.api.resource;
 
 import brachy84.brachydium.Brachydium;
-import brachy84.brachydium.api.render.MbeHolderModel;
-import net.fabricmc.fabric.api.client.model.ModelProviderContext;
-import net.fabricmc.fabric.api.client.model.ModelProviderException;
-import net.fabricmc.fabric.api.client.model.ModelResourceProvider;
-import net.minecraft.client.render.model.UnbakedModel;
+import net.fabricmc.fabric.api.client.model.ExtraModelProvider;
+import net.minecraft.client.util.ModelIdentifier;
+import net.minecraft.resource.ResourceManager;
 import net.minecraft.util.Identifier;
-import org.jetbrains.annotations.Nullable;
 
-public class ModelProvider implements ModelResourceProvider {
+import java.util.function.Consumer;
 
-    public static final MbeHolderModel META_BLOCK_ENTITY_MODEL = new MbeHolderModel();
+public class ModelProvider implements ExtraModelProvider {
 
     @Override
-    public @Nullable UnbakedModel loadModelResource(Identifier identifier, ModelProviderContext modelProviderContext) {
-        if(identifier.getNamespace().equals(Brachydium.MOD_ID)) {
-            if(identifier.getPath().equals("block/generic_mbe") || identifier.getPath().startsWith("item/mbe/")) {
-                return META_BLOCK_ENTITY_MODEL;
-            }
-        }
-        if(identifier.equals(Brachydium.id("block/generic_mbe")))
-            return META_BLOCK_ENTITY_MODEL;
-        return null;
+    public void provideExtraModels(ResourceManager manager, Consumer<Identifier> out) {
+        out.accept(new ModelIdentifier(Brachydium.id("bucket_base"), "inventory"));
+        out.accept(new ModelIdentifier(Brachydium.id("bucket_fluid"), "inventory"));
+        out.accept(new ModelIdentifier(Brachydium.id("bucket_background"), "inventory"));
     }
 }
