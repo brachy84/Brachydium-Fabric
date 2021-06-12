f312eac6601c387057eb424b7caee7a316263f88
brachy84 <brachy84@gmail.com>
2021-06-12 18:00:00 +0200

    restructure main class

--- src/main/java/brachy84/brachydium/Brachydium.java
+++ src/main/java/brachy84/brachydium/Brachydium.java
@@ -1,21 +1,23 @@
 package brachy84.brachydium;
 
 import brachy84.brachydium.api.BrachydiumInitializer;
-import brachy84.brachydium.api.blockEntity.MTBlockEntities;
-import brachy84.brachydium.api.blockEntity.MetaBlockEntityUIFactory;
-import brachy84.brachydium.api.item.tool.Tools;
+import brachy84.brachydium.api.blockEntity.old.MetaBlockEntityUIFactory;
+import brachy84.brachydium.api.material.Material;
 import brachy84.brachydium.api.recipe.RecipeLoadEvent;
-import brachy84.brachydium.api.recipe.RecipeTables;
 import brachy84.brachydium.api.render.Textures;
 import brachy84.brachydium.api.resource.RRPHelper;
+import brachy84.brachydium.api.resource.ResourceReloadListener;
 import brachy84.brachydium.gui.ServerUi;
 import net.devtech.arrp.api.RRPCallback;
 import net.devtech.arrp.api.RuntimeResourcePack;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.resource.ResourceType;
 import net.minecraft.util.Identifier;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.jetbrains.annotations.ApiStatus;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,6 +29,10 @@ public class Brachydium implements ModInitializer {
     public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
     public static final RuntimeResourcePack RESOURCE_PACK = RuntimeResourcePack.create(MOD_ID + ":generated");
 
+    private static String currentRegisteringMod = "NONE";
+
+    private static boolean tagsLoaded = false;
+
     public static Identifier id(String path) {
         return new Identifier(MOD_ID, path);
     }
@@ -35,25 +41,50 @@ public class Brachydium implements ModInitializer {
 
     @Override
     public void onInitialize() {
+        ResourceManagerHelper.get(ResourceType.SERVER_DATA).registerReloadListener(ResourceReloadListener.INSTANCE);
+        ServerUi.init();
+        Textures.init();
+        RecipeLoadEvent.EVENT.register(Material::runProcessors);
         plugins.addAll(FabricLoader.getInstance().getEntrypoints("brachydium", BrachydiumInitializer.class));
         System.out.println("--------------------------------------------------");
         for(BrachydiumInitializer plugin : plugins) {
+            currentRegisteringMod = plugin.getModId();
             RecipeLoadEvent.EVENT.register(plugin::registerRecipes);
             plugin.registerMaterials();
+        }
+        currentRegisteringMod = "NONE";
+        Material.REGISTRY.freeze();
+        Material.registerItems();
+        Material.registerResources();
+        for(BrachydiumInitializer plugin : plugins) {
+            currentRegisteringMod = plugin.getModId();
             plugin.registerGeneral();
         }
-        ServerUi.init();
-        Textures.init();
-        RecipeTables.init();
-        Tools.register();
+        currentRegisteringMod = "NONE";
+
         MetaBlockEntityUIFactory.INSTANCE.init();
-        MTBlockEntities.init();
 
         RRPHelper.initOtherResources();
         //RESOURCE_PACK.dump(new File("brachydium_assets"));
         RRPCallback.EVENT.register(a -> a.add(RESOURCE_PACK));
         System.out.println("--------------------------------------------------");
-
-
     }
+
+    public static boolean currentPluginIsNone() {
+        return currentRegisteringMod.equals("NONE");
+    }
+
+    public static String getCurrentPlugin() {
+        return currentPluginIsNone() ? "" : currentRegisteringMod;
+    }
+
+    @ApiStatus.Internal
+    public static void setTagsLoaded() {
+        tagsLoaded = true;
+    }
+
+    public static boolean areTagsLoaded() {
+        return tagsLoaded;
+    }
+
 }
