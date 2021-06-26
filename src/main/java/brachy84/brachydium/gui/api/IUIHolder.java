cd74a6b15b905285dbdfe4e68faa8633997e9522
brachy84 <brachy84@gmail.com>
2021-06-26 06:58:05 +0200

    add comment

--- src/main/java/brachy84/brachydium/gui/api/IUIHolder.java
+++ src/main/java/brachy84/brachydium/gui/api/IUIHolder.java
@@ -6,6 +6,11 @@ import org.jetbrains.annotations.NotNull;
 
 public interface IUIHolder {
 
+    /**
+     * if this returns false then {@link #createUi(PlayerEntity)} can return null
+     * @return if the holder has a ui
+     * if it returns true, it will call {@link #createUi(PlayerEntity)}
+     */
     boolean hasUI();
 
     @NotNull
