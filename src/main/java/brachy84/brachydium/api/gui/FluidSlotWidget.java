7bf1b1893be59ad40dde74176db7214e739fdc24
brachy84 <brachy84@gmail.com>
2021-11-20 21:35:57 +0100

    fix fluid slot widget

--- src/main/java/brachy84/brachydium/api/gui/FluidSlotWidget.java
+++ src/main/java/brachy84/brachydium/api/gui/FluidSlotWidget.java
@@ -1,20 +1,34 @@
 package brachy84.brachydium.api.gui;
 
+import brachy84.brachydium.Brachydium;
 import brachy84.brachydium.api.fluid.FluidStack;
 import brachy84.brachydium.api.handlers.storage.IFluidHandler;
-import brachy84.brachydium.gui.api.GuiHelper;
-import brachy84.brachydium.gui.api.ITexture;
+import brachy84.brachydium.api.util.TransferUtil;
 import brachy84.brachydium.gui.api.math.AABB;
 import brachy84.brachydium.gui.api.math.Pos2d;
 import brachy84.brachydium.gui.api.math.Size;
+import brachy84.brachydium.gui.api.rendering.GuiHelper;
+import brachy84.brachydium.gui.api.rendering.ITexture;
 import brachy84.brachydium.gui.api.widgets.ResourceSlotWidget;
 import com.google.common.collect.Lists;
 import me.shedaniel.rei.api.client.gui.widgets.Widget;
 import me.shedaniel.rei.api.client.gui.widgets.Widgets;
+import net.fabricmc.fabric.api.transfer.v1.context.ContainerItemContext;
+import net.fabricmc.fabric.api.transfer.v1.fluid.FluidStorage;
+import net.fabricmc.fabric.api.transfer.v1.fluid.FluidVariant;
+import net.fabricmc.fabric.api.transfer.v1.storage.Storage;
+import net.fabricmc.fabric.api.transfer.v1.storage.StorageView;
+import net.fabricmc.fabric.api.transfer.v1.transaction.Transaction;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.item.BucketItem;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,6 +37,7 @@ import java.util.Objects;
 public class FluidSlotWidget extends ResourceSlotWidget<FluidStack> {
 
     private static final Size SIZE = new Size(18, 18);
+    private static final Size FLUID_SIZE = new Size(16, 16);
     private final IFluidHandler fluidHandler;
     private final int index;
     private byte mark;
@@ -36,8 +51,20 @@ public class FluidSlotWidget extends ResourceSlotWidget<FluidStack> {
 
     @Override
     public void renderResource(MatrixStack matrices, Pos2d mousePos) {
-        GuiHelper.drawFluid(matrices, getResource().getFluid(), getResource().getAmount() + "mb", getPos().add(new Pos2d(1, 1)), new Size(16, 16));
+        GuiHelper.drawFluid(matrices, getResource().asFluidVariant(), getPos().add(1, 1), FLUID_SIZE);
+        drawFluidAmount(matrices, getResource(), getPos().add(17f, 12.5f), 0.45f);
 
+    }
+
+    public static void drawFluidAmount(MatrixStack matrices, FluidStack stack, Pos2d pos, float scale) {
+        matrices.push();
+        matrices.scale(scale, scale, 1);
+        TextRenderer textRenderer = MinecraftClient.getInstance().textRenderer;
+        String amountText = stack.getAmountDisplay();
+        float y = pos.y / scale, x = pos.x / scale;
+        x -= textRenderer.getWidth(amountText);
+        textRenderer.draw(matrices, amountText, x, y, 0xFFFFFF);
+        matrices.pop();
     }
 
     @Override
@@ -46,43 +73,26 @@ public class FluidSlotWidget extends ResourceSlotWidget<FluidStack> {
     }
 
     @Override
-    public void readData(boolean fromServer, PacketByteBuf data) {
-        setResource(FluidStack.readData(data));
+    public void readClientData(int id, PacketByteBuf buf) {
+        if (id == 0)
+            onClickServer();
     }
 
     @Override
-    public void writeData(boolean fromServer, PacketByteBuf data) {
-        getResource().writeData(data);
+    public void readServerData(int id, PacketByteBuf buf) {
+        if (id == 0)
+            setResource(FluidStack.readData(buf));
     }
 
     @Override
     public FluidStack getResource() {
-        return fluidHandler.getStackAt(index);
+        return fluidHandler.getFluid(index);
     }
 
     @Override
     public boolean setResource(FluidStack resource) {
-        fluidHandler.setStack(index, resource);
+        fluidHandler.setFluid(index, resource);
         return true;
-    }
-
-    /*public long insert(FluidStack stack, TransactionContext transactionContext) {
-        return fluidSlot.insert(FluidVariant.of(stack.getFluid(), stack.getNbt()), stack.getAmount(), transactionContext);
-    }
-
-    public FluidStack extract(long amount, TransactionContext transactionContext) {
-        FluidStack stack = getResource();
-        long extrcated = fluidSlot.extract(FluidVariant.of(stack.getFluid(), stack.getNbt()), amount, transactionContext);
-        if(extrcated == 0) return FluidStack.EMPTY;
-        return stack.copyWith((int) extrcated);
-    }*/
-
-    private void setCursorStack(ItemStack stack) {
-        getGui().setCursorStack(stack);
-    }
-
-    private ItemStack getCursorStack() {
-        return getGui().getCursorStack();
     }
 
     @Override
@@ -105,64 +115,76 @@ public class FluidSlotWidget extends ResourceSlotWidget<FluidStack> {
         return this;
     }
 
-   /* @Override
+    public void onClickServer() {
+        FluidStack currentFluid = getResource();
+        long cap = fluidHandler.getCapacityAt(index);
+        if (!currentFluid.isEmpty())
+            cap = cap - currentFluid.getAmount();
+        ItemStack cursorStack = getGui().getCursorStack();
+        boolean didInsertToSlot = false;
+        if (!cursorStack.isEmpty() && cursorStack.getCount() == 1) {
+            // todo: use #withPlaySlot() for better compat
+            ContainerItemContext itemContext = ContainerItemContext.withInitial(getGui().getCursorStack());
+            Storage<FluidVariant> storage = itemContext.find(FluidStorage.ITEM);
+            if (storage == null)
+                return;
+            if (mark != 2) {
+                try (Transaction transaction = Transaction.openOuter()) {
+                    for (StorageView<FluidVariant> storageView : storage.iterable(transaction)) {
+                        if (storageView.isResourceBlank())
+                            continue;
+                        FluidVariant extractable = storageView.getResource();
+                        if (currentFluid.isEmpty() || currentFluid.matches(extractable)) {
+                            long extracted = storageView.extract(extractable, Math.min(storageView.getAmount(), cap), transaction);
+                            if (extracted == 0)
+                                continue;
+                            if (currentFluid.isEmpty())
+                                setResource(new FluidStack(extractable, extracted));
+                            else {
+                                currentFluid.increment(extracted);
+                                // trigger update
+                                setResource(currentFluid);
+                            }
+                            didInsertToSlot = true;
+                            if (cursorStack.getItem() instanceof BucketItem) {
+                                getGui().setCursorStack(new ItemStack(Items.BUCKET));
+                            }
+                            break;
+                        }
+                        transaction.commit();
+                    }
+                }
+            }
+            if (!didInsertToSlot && !currentFluid.isEmpty()) {
+                try (Transaction transaction = Transaction.openOuter()) {
+                    long inserted = storage.insert(currentFluid.asFluidVariant(), currentFluid.getAmount(), transaction);
+                    if (inserted > 0) {
+                        if (inserted == currentFluid.getAmount())
+                            setResource(FluidStack.EMPTY);
+                        else {
+                            currentFluid.decrement(inserted);
+                            // trigger update
+                            setResource(currentFluid);
+                        }
+                        if (cursorStack.getItem() instanceof BucketItem) {
+                            Item bucket = TransferUtil.getBucketItem(currentFluid.getFluid());
+                            if (bucket != null)
+                                getGui().setCursorStack(new ItemStack(bucket));
+                            else
+                                Brachydium.LOGGER.error("Could not find filled bucket for {}", currentFluid.getFluid().getDefaultState().getBlockState().getBlock().getName());
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
     public ActionResult onClick(Pos2d pos, int buttonId, boolean isDoubleClick) {
-        ItemStack cursorStack = getCursorStack();
-
-        if (cursorStack.getCount() == 1 && cursorStack.getItem() instanceof BucketItemAccess_AccessFluid) {
-            Fluid fluid = ((BucketItemAccess_AccessFluid) cursorStack.getItem()).getFluid();
-            if (fluid == Fluids.EMPTY && !isEmpty()) {
-                fillBucket(fluidSlot);
-            } else if (fluid != Fluids.EMPTY) {
-                emptyBucket(cursorStack, fluidSlot);
-            }
-        }
-
-        sendToClient((ServerPlayerEntity) getGui().player);
-        return ActionResult.PASS;
+        syncToServer(0, buf -> {
+        });
+        return ActionResult.SUCCESS;
     }
-
-    public boolean emptyBucket(ItemStack stack, Insertable<Fluid> insertable) {
-        Fluid fluid = BucketHelper.getFluid(stack.getItem());
-        if (fluid != Fluids.EMPTY) {
-            try (Transaction transaction = Transaction.create()) {
-                if (insertable.insert(transaction, fluid, Amounts.BUCKET) != Amounts.BUCKET) {
-                    transaction.abort();
-                    return false;
-                }
-            }
-            setCursorStack(new ItemStack(Items.BUCKET));
-            return true;
-        }
-        return false;
-    }
-
-    public boolean fillBucket(Extractable<Fluid> extractable) {
-        Fluid fluid;
-        SingleFluidTank tank = new SingleFluidTank(1);
-        try (Transaction transaction = Transaction.create()) {
-            extractable.extract(transaction, tank);
-            fluid = tank.getKey(transaction);
-            transaction.abort();
-        }
-        if (fluid != null && fluid != Fluids.EMPTY) {
-            try (Transaction transaction = Transaction.create()) {
-                int extracted = extractable.extract(transaction, fluid, Amounts.BUCKET);
-                if (extracted != Amounts.BUCKET) {
-                    transaction.abort();
-                    return false;
-                }
-                //TODO: This might get laggy with a lot of items
-                Optional<Item> bucket = BucketHelper.getBucketForFluid(fluid);
-                if (bucket.isPresent()) {
-                    setCursorStack(new ItemStack(bucket.get()));
-                    return true;
-                }
-                transaction.abort();
-            }
-        }
-        return false;
-    }*/
 
     @Override
     public List<Widget> getReiWidgets(AABB bounds, Pos2d reiPos) {
@@ -170,7 +192,6 @@ public class FluidSlotWidget extends ResourceSlotWidget<FluidStack> {
         me.shedaniel.rei.api.client.gui.widgets.Slot slot = Widgets.createSlot(reiPos.add(new Pos2d(1, 1)).asReiPoint());
         slot.backgroundEnabled(false);
         slot.setNoticeMark(mark);
-        widgets.add(slot);
         me.shedaniel.rei.api.client.gui.widgets.Widget render = Widgets.createDrawableWidget(((helper, matrices, mouseX, mouseY, delta) -> {
             if (getTextures().size() > 0) {
                 for (ITexture drawable : getTextures()) {
@@ -181,6 +202,7 @@ public class FluidSlotWidget extends ResourceSlotWidget<FluidStack> {
             }
         }));
         widgets.add(render);
+        widgets.add(slot);
         return widgets;
     }
 }
