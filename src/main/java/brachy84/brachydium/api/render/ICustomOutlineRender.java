cc280995d9cc3ba33f167e7c7ecae2bb47294b9c
brachy84 <brachy84@gmail.com>
2021-11-21 16:21:56 +0100

    doc for ICustomOutlineRender

--- src/main/java/brachy84/brachydium/api/render/ICustomOutlineRender.java
+++ src/main/java/brachy84/brachydium/api/render/ICustomOutlineRender.java
@@ -13,16 +13,23 @@ import net.minecraft.util.math.Vec3i;
 
 import java.util.List;
 
+/**
+ * Implement this on a Block class to get a outline render callback
+ */
 public interface ICustomOutlineRender {
 
     /**
      * Render extra block outline
-
+     *
      * @return true if the default outline should be rendered too
      */
     @Environment(EnvType.CLIENT)
     boolean renderOutline(MatrixStack matrices, VertexConsumer vertexConsumer, double camX, double camY, double camZ, BlockState state, BlockPos pos);
 
+    /**
+     * A helper method to render Edges that can be transformed
+     * Supposed to be called from {@link #renderOutline(MatrixStack, VertexConsumer, double, double, double, BlockState, BlockPos)}
+     */
     default void drawEdges(List<Edge> edges, MatrixStack matrices, VertexConsumer vertexConsumer, double camX, double camY, double camZ, Vec3i pos) {
         MatrixStack.Entry entry = matrices.peek();
         double drawX = pos.getX() - camX, drawY = pos.getY() - camY, drawZ = pos.getZ() - camZ;
@@ -31,10 +38,17 @@ public interface ICustomOutlineRender {
         });
     }
 
+    /**
+     * A helper class to render lines, which can also be transformed to other sides;
+     */
     class Edge {
         private double startX = 0D, startY = 0D, startZ = 0D, endX = 0D, endY = 0D, endZ = 0D;
         private float red = 0f, green = 0f, blue = 0f, alpha = 0.4f;
 
+        /**
+         * Sets the start pos of the edge.
+         * Should be values between 0 and 1
+         */
         public Edge start(double x, double y, double z) {
             this.startX = x;
             this.startY = y;
@@ -42,6 +56,10 @@ public interface ICustomOutlineRender {
             return this;
         }
 
+        /**
+         * Sets the end pos of the edge.
+         * Should be values between 0 and 1
+         */
         public Edge end(double x, double y, double z) {
             this.endX = x;
             this.endY = y;
@@ -49,6 +67,10 @@ public interface ICustomOutlineRender {
             return this;
         }
 
+        /**
+         * Sets the color of the drawn edge
+         * Should be values between 0 and 1
+         */
         public Edge color(float red, float green, float blue, float alpha) {
             this.red = red;
             this.green = green;
@@ -57,32 +79,51 @@ public interface ICustomOutlineRender {
             return this;
         }
 
+        /**
+         * Sets the color of the drawn edge
+         */
         public Edge color(Color color) {
             return color(color.getRed() / 255f, color.getGreen() / 255f, color.getBlue() / 255f, color.getAlpha() / 255f);
         }
 
+        /**
+         * Renders the edge
+         */
         public void render(MatrixStack.Entry entry, VertexConsumer vertexConsumer, double x, double y, double z) {
-            float q = (float)(endX - startX);
-            float r = (float)(endY - startY);
-            float s = (float)(endZ - startZ);
+            float q = (float) (endX - startX);
+            float r = (float) (endY - startY);
+            float s = (float) (endZ - startZ);
             float t = MathHelper.sqrt(q * q + r * r + s * s);
             q /= t;
             r /= t;
             s /= t;
-            vertexConsumer.vertex(entry.getModel(), (float)(startX + x), (float)(startY + y), (float)(startZ + z)).color(red, green, blue, alpha).normal(entry.getNormal(), q, r, s).next();
-            vertexConsumer.vertex(entry.getModel(), (float)(endX + x), (float)(endY + y), (float)(endZ + z)).color(red, green, blue, alpha).normal(entry.getNormal(), q, r, s).next();
+            vertexConsumer.vertex(entry.getModel(), (float) (startX + x), (float) (startY + y), (float) (startZ + z)).color(red, green, blue, alpha).normal(entry.getNormal(), q, r, s).next();
+            vertexConsumer.vertex(entry.getModel(), (float) (endX + x), (float) (endY + y), (float) (endZ + z)).color(red, green, blue, alpha).normal(entry.getNormal(), q, r, s).next();
         }
 
+        /**
+         * Creates an exact copy and transforms it via {@link #transform(Direction, Direction)}
+         *
+         * @param from what the data currently is on
+         * @param to   the result side
+         * @return the copied and transformed edge
+         */
         public Edge copyAndTransform(Direction from, Direction to) {
             Edge edge = new Edge().start(startX, startY, startZ).end(endX, endY, endZ).color(red, green, blue, alpha);
             edge.transform(from, to);
             return edge;
         }
 
+        /**
+         * Transforms the edge from one direction to another
+         *
+         * @param from what the data currently is on
+         * @param to   the result side
+         */
         public void transform(Direction from, Direction to) {
-            if(from == to)
+            if (from == to)
                 return;
-            if(from.getAxis() != to.getAxis()) {
+            if (from.getAxis() != to.getAxis()) {
                 double temp = getStart(from.getAxis());
                 setStart(from.getAxis(), getStart(to.getAxis()));
                 setStart(to.getAxis(), temp);
@@ -92,7 +133,7 @@ public interface ICustomOutlineRender {
                 setEnd(to.getAxis(), temp);
             }
 
-            if(from.getDirection() != to.getDirection()) {
+            if (from.getDirection() != to.getDirection()) {
                 setStart(to.getAxis(), 1 - getStart(to.getAxis()));
                 setEnd(to.getAxis(), 1 - getEnd(to.getAxis()));
             }
